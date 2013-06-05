cbb0e5aeb09003d64813a56e9f7650f60b1efee6
Kien Pham <kien@sendgrid.com>
2013-06-05 13:48:44 -0700

    init commit

---
+++ Hotelx/Hotelx/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Hotelx
+//
+//  Created by Kien Pham on 6/5/13.
+//  Copyright (c) 2013 Hotelx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
